96d9ca03f5a052f15042b31bf15c8fbdd81f26d1
Ray Smets <rayjsmets@gmail.com>
2023-11-26 22:04:03 -0800

    footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script>
 	import Button from '$lib/components/Button.svelte';
+	import Header from '../lib/components/Header.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 </script>
 
-<!-- <style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style> -->
-
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<Header />
+<h1 class="text-red-500 text-4xl">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <Button text="Click Me" />
+<Footer />
+
