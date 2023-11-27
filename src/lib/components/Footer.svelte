23be4e00846814be48a0948a1786dbd83564d631
Ray Smets <rayjsmets@gmail.com>
2023-11-26 22:06:11 -0800

    [Components] added (#2)
    * Button
    
    * static
    
    * header
    
    * trunk
    
    * header
    
    * trunk
    
    * cartButton
    
    * footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<footer class="flex justify-between py-6 border-t border-neutral-400 text-neutral-400">
+	<p>Made with ❣️</p>
+	<p>Created by rsmets</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '$lib/components/Button.svelte';
+	import Header from '../lib/components/Header.svelte';
+	import Footer from '../lib/components/Footer.svelte';
+</script>
 
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style>
+<Header />
+<h1 class="text-red-500 text-4xl">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button text="Click Me" />
+<Footer />
+
