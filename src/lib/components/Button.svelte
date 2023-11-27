f5b60001d27309700fe7c4060f620cd1381a0976
Ray Smets <rayjsmets@gmail.com>
2023-11-26 21:26:49 -0800

    Button

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,10 @@
+<script>
+    /**
+	 * @type {string}
+	 */
+     export let text;
+</script>
+
+<button on:click={() => console.log('Button clicked!')}>
+    {text}
+</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
-<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Button from '$lib/components/Button.svelte';
+</script>
 
-<style lang="postcss">
+<!-- <style lang="postcss">
 	:global(html) {
 		background-color: theme(colors.gray.100);
 	}
-</style>
+</style> -->
+
+<h1 class="text-3xl font-bold underline">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button text="Click Me"/>
